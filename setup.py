ed4e1920a5c1f27bdb1134141f3d4e9862c78155
Chua Keng Koon <kengkoon.chua@gmail.com>
2026-03-25 23:31:41 +0800

    v0.96 Fix AgentPanel combo not populated

--- setup.py
+++ setup.py
@@ -4,6 +4,14 @@ setup(
     name="kk-agent-skills",
     version="1.0.0",
     packages=find_packages(),
+    package_data={
+        "kk_agent_skills": [
+            "*/config.json",
+            "*/adapters/**/*.json",
+            "*/adapters/**/*.yaml",
+        ],
+    },
+    include_package_data=True,
     install_requires=[
         "kk-utils",
     ],
